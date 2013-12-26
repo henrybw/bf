d215216ab38bc4e91c3434f8fcbf011d924ade12
htbw <henry.babaweiss@gmail.com>
2013-12-25 18:00:44 -0800

    Fix issue #1 to handle nested loops correctly, along with refactoring and cleanup.
    This adds an instruction buffer (instead of maintaining a pointer into
    the file itself) and a stack (to correctly track nested loops) to the
    interpreter's runtime system. Code has been significantly refactored as
    a result.
    
    Also removed the samples from the repo, as I did not write those
    programs and don't want to infringe on any license restrictions.

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 /*
- * bf: a brainfuck interpreter
- * v1.0, by Henry Weiss
- * 10/28/08
+ * bf: a simple 8-bit brainfuck interpreter
+ * v2.0
  */
 
 #include <stdio.h>
@@ -9,113 +8,193 @@
 #include <string.h>
 
 #define MEM_SLOTS 30000
+#define STACK_PAGE 32  /* Number of stack levels to track before reallocating */
 
 /* Data storage */
+char *instrs;
+size_t *stack;  /* For handling loops. Array of offsets into instrs. */
 unsigned char memory[MEM_SLOTS];
-unsigned int ptr = 0;
+
+/* Runtime state */
+size_t ip;  /* Points to next instr to be executed */
+size_t sp;  /* Points to next free stack slot (top of stack is at sp-1) */
+size_t stacksize;
+unsigned short dp;
 
 /* Function defs */
-void parsechar(char ch, FILE *file);
-void handleloop(FILE *file);
+void init(const char *filename);
+char *loadinstrs(FILE *file);
+void interpret(char ch);
+void handlejump(char jumpinst);
+void cleanup();
 
 int main(int argc, const char *argv[])
 {
-    /* Check for valid args */
+    char inst;
+
     if (argc < 2)
     {
         printf("Usage: bf [source file]\n");
         exit(1);
     }
+
+    init(argv[1]);
     
-    /* Open the file for reading */
-    FILE *file = fopen(argv[1], "r");
-    
-    if (!file)
-    {
-        fprintf(stderr, "File \"%s\" could not be opened.\n", argv[1]);
-        exit(1);
-    }
-    
-    /* Initialize the memory slots */
-    int i;
-    
-    for (i = 0; i < MEM_SLOTS; i++)
-        memory[i] = 0;
-    
-    /* Loop through chars and parse the tokens */
-    char ch;
-    
-    while ((ch = fgetc(file)) != EOF)
-        parsechar(ch, file);
-    
-    /* Clean up */
-    fclose(file);
+    /* Main execution loop: fetch next instruction from the instruction
+       buffer and interpret it. */
+    while ((inst = instrs[ip++]) != 0)
+        interpret(inst);
     
     return 0;
 }
 
-/* Parses one character and takes appropriate action if it's a token */
-void parsechar(char ch, FILE *file)
+/* Initialize runtime state and load instructions from the given source file */
+void init(const char *filename)
 {
-    switch (ch)
+    FILE *file;
+
+    file = fopen(filename, "r");
+    if (!file)
+    {
+        fprintf(stderr, "File \"%s\" could not be opened.\n", filename);
+        exit(1);
+    }
+    instrs = loadinstrs(file);
+    ip = 0;
+    fclose(file);
+
+    stack = (size_t *)calloc(STACK_PAGE, sizeof(size_t));
+    stacksize = STACK_PAGE;
+    sp = 0;
+
+    memset(memory, 0, MEM_SLOTS);
+    dp = 0;
+}
+
+/* Loads instructions from the given source file. Allocates and returns a
+   buffer containing instructions read from the file. */
+char *loadinstrs(FILE *file)
+{
+    long filesize;
+    char *buff;
+    char *buffptr;
+    char ch;
+
+    /* Calculate file size to determine the size of the instruction buffer */
+    fseek(file, 0, SEEK_END);
+    filesize = ftell(file);
+    rewind(file);
+    
+    /* Make the instruction buffer null-terminated by calloc'ing one more
+       byte than necessary for the instructions. */
+    buff = (char *)calloc(filesize + 1, sizeof(char));
+    if (!buff)
+    {
+        fprintf(stderr, "Failed to allocate the instruction buffer.\n");
+        exit(1);
+    }
+
+    /* Read instructions from the file */
+    buffptr = buff;
+    while ((ch = fgetc(file)) != EOF)
+        *buffptr++ = ch;
+
+    return buff;
+}
+
+/* Parses one instruction */
+void interpret(char inst)
+{
+    switch (inst)
     {
         case '>':
-            ptr++;
+            dp++;
             break;
             
         case '<':
-            ptr--;
+            dp--;
             break;
             
         case '+':
-            memory[ptr]++;
+            memory[dp]++;
             break;
             
         case '-':
-            memory[ptr]--;
+            memory[dp]--;
             break;
             
         case '.':
-            putchar(memory[ptr]);
+            putchar(memory[dp]);
             break;
             
         case ',':
-            memory[ptr] = getchar();
+            memory[dp] = getchar();
             break;
             
         case '[':
-            handleloop(file);
+        case ']':
+            handlejump(inst);
             break;
     }
 }
 
-/* Handles all execution inside a loop. Nested loops are handled recursively,
-   since parsechar() will call this again if another loop token is found. */
-void handleloop(FILE *file)
+/* Manipulate control flow based on the given jump instruction */
+void handlejump(char jumpinst)
 {
-    char ch;
-    fpos_t loopstart;
-    
-    /* Check if we should even enter the loop in the first place */
-    if (memory[ptr])
+    if (jumpinst == '[')
     {
-        /* Save the position of the loop start */
-        fgetpos(file, &loopstart);
-        
-        /* Set up the loop execution */
-        while (memory[ptr])
+        if (memory[dp])
         {
-            /* Go to the top of the loop again */
-            fsetpos(file, &loopstart);
-            
-            /* Actually execute the loop body now */
-            while ((ch = fgetc(file)) != ']')
-                parsechar(ch, file);
+            /* We are going to execute the loop body: save the loop start
+               onto the stack for when we encounter the loop end. */
+            stack[sp++] = ip;
+
+            if (sp >= stacksize)
+            {
+                stacksize *= 2;
+                stack = (size_t *)realloc(stack, stacksize);
+            }
+        }
+        else
+        {
+            /* We are skipping this loop: find the bracket that matches the
+               bracket we are currently processing, accounting for nesting. */
+            int nestdepth = 0;
+            int currentip = ip - 1;  /* So we process the original '[' first */
+
+            do
+            {
+                char inst = instrs[currentip++];
+
+                if (inst == '[')
+                    nestdepth++;
+                else if (inst == ']')
+                    nestdepth--;
+            }
+            while (nestdepth > 0);
+
+            ip = currentip;
         }
     }
-    else
+    else if (jumpinst == ']')
     {
-        while ((ch = fgetc(file)) != ']')
-            ;  /* Skip over the loop */
+        if (memory[dp])
+        {
+            /* Jump back to the beginning of the loop body */
+            ip = stack[sp - 1];
+        }
+        else
+        {
+            /* Exit loop body and pop this loop from the stack */
+            sp--;
+        }
     }
 }
+
+/* Frees all globally allocated resources */
+void cleanup()
+{
+    free(instrs);
+    free(stack);
+}
+
